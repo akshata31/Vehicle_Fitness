061f6df620471b47823f569b64810b52854a72be
Akshata Anand Atnoorkar <66873893+akshata31@users.noreply.github.com>
2022-10-11 22:32:16 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vehicle Fitness Testing</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+<body>
+    <!---------------NAVBAR------------------>
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <img src="img/logo1.webp" style="margin-top:10px; width:40px;margin-bottom:10px;margin-left:-10px; align:center" class="img-responsive" alt="Vehicle Fitness Testing">
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li> <a href="#" style="color:white" >HOME</a></li>
+                        <li> <a href="#about-us"style="color:white"> ABOUT US</a></li>
+												<li> <a href="#how-to-apply" style="color:white">HOW TO APPLY</a> </li>
+												<li> <a href="#contact" style="color:white">CONTACT </a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="login.php" style="color:white"><span class="glyphicon glyphicon-user"></span>Login as User</a></li>
+                        <li><a href="login.php" style="color:white"><span class="glyphicon glyphicon-log-in"></span> Login as Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+<!--------------------BANNER-------------------->
+<section id="banner">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="bannertitle"> Getting Vehicle Fitness Certificate</p>
+                <p class="bannertitlep">If you are thinking about how to get vehicle fitness certificate online , you are at right page.</p>
+                <a href="login.php">
+                    <button type="button" class="btn btn-primary">SCHEDULE NOW!</button>
+                </a>
+            </div>
+            <div class="col-md-6 text-center">
+                <img src="img/img6.svg" class="img-fluid" style="margin-top:45px; width:30em; margin-bottom:10px;margin-left:-10px;" class="img-responsive">
+            </div>
+        </div>
+    </div>
+    <img src="img/wave1.png" class="bottom-img">
+</section>
+<!--------------------ABOUT US-------------------->
+<section id="about-us">
+    <div class="container">
+        <h1 class="title text-center">ABOUT US</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <h4 class="title-1">What is a Fitness Certificate of a vehicle?</h4>
+                <p class="about-para"><a href="https://morth.nic.in/">The Ministry of Road Transport and Highways (MORTH)</a> has laid down laws that require vehicle owners to acquire specific certificates for their vehicle, linked to its health and operation, in order to guarantee that cars that operate on Indian roads perform optimally.<br />
+									A certificate of fitness for vehicles is a government-issued document that certifies that the vehicle is safe to drive on the road in terms of its general condition. Commercial, as well as private automobiles, are required by law to have a fitness certificate.
+								</p>
+            </div>
+            <div class="col-md-6">
+                <h4 class="title-1">Purposes</h4>
+								<p class="about-para">
+									<ul style="color: #00004d">
+ 									 <li style="font-size:17px; color:">To check and monitor the pollution caused by the emission of smoke from your automobile. This is critical in light of the ongoing issue of rising pollution as a result of car smoke emissions.
+ 									</li>
+ 									<li style="font-size:17px;">To maintain and operate your car in a safe and appropriate manner.</li>
+ 									<li style="font-size:17px;">
+ 										Last but not least, to avoid traffic accidents that may arise as a result of your vehicle's poor condition.
+ 									</li>
+ 								</ul>
+							</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------How to APPLY------------------>
+<section id="how-to-apply" style="background-color:#d9e5f2 ";>
+    <div class="container">
+        <h1 class="title text-center">HOW TO APPLY</h1>
+        <div class="row">
+            <div class="col-md-12">
+                <ul style="margin:10px; list-style-type: square";>
+									<li style="margin:10px; font-size:17px; color: #00004d";>
+										STEP 1:Visit the Vehicle Fitness Testing website on the browser of your smartphone or laptop.
+									</li>
+									<li style="margin:10px; font-size:17px; color: #00004d";>
+										STEP 2:If you do not have an account, create one and submit the details of your vehicle, such as registration number.
+									</li>
+									<li style="margin:10px; font-size:17px; color: #00004d";>
+										 STEP 3:Now select the option of application for vehicle fitness certificate, and enter your vehicle’s chassis number along with your registered mobile number.
+									</li>
+									<li style="margin:10px; font-size:17px; color: #00004d";>
+										STEP 4:Visit the RTO with your vehicle and relevant documents and complete the vehicle inspection.
+									</li>
+									<li style="margin:10px; font-size:17px; margin-bottom:10px; color: #00004d";>
+										STEP 5:Once all the process is completed with each and every testcases passed you will be provided with a certificate.
+									</li>
+								</ul>
+            </div>
+        </div>
+				<div class="col-mb-12";>
+					<center>
+						<iframe width="420" height="315"
+							 src="https://www.youtube.com/embed/bcT2N5El200">
+						</iframe>
+					</center>
+				</div>
+    </div>
+</section>
+
+
+
+<!--------------------CONTACT-------------------->
+<section id="contact">
+    <div class="container">
+        <h1 class="title text-center">CONTACT</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <p class="about-para1"><span class="glyphicon glyphicon-envelope"></span> Email :admin@gmail.com<br>
+                    <span class="glyphicon glyphicon-earphone"></span> Mobile :123456789 </p>
+            </div>
+            <div class="col-md-6">
+                <p class="about-para2"><span class="glyphicon glyphicon-map-marker"></span> MGM's College Of Engineering
+                 <br> Airport Road ,Nanded-431601 </p>
+            </div>
+        </div>
+    </div>
+</section>
+<p class="about-para3" >Copyright<span class="glyphicon glyphicon-copyright-mark"></span> Akshata and Navneeta</p>
+</body>
+</html>
